481c226364168385ef4c3b0c65cbb0c84b54036a
Korusuke <karan.sheth@somaiya.edu>
2019-05-23 05:18:44 +0530

    Add Archive type keyword and tests
    Fixes: #3

--- upt_macports/tests/test_macports_package.py
+++ upt_macports/tests/test_macports_package.py
@@ -32,5 +32,26 @@ class TestMacPortsPackageLicenses(unittest.TestCase):
         self.assertEqual(self.package.licenses, expected)
 
 
+class TestMacPortsPackageArchiveType(unittest.TestCase):
+    def setUp(self):
+        self.package = MacPortsPackage()
+        self.package.upt_pkg = upt.Package('foo', '42')
+
+    def test_no_archive(self):
+        self.package.upt_pkg.archives = []
+        expected = 'unknown'
+        self.assertEqual(self.package.archive_type, expected)
+
+    def test_known_archive(self):
+        self.package.upt_pkg.archives = [upt.Archive("url.co/dir/file.tar.gz")]
+        expected = 'gz'
+        self.assertEqual(self.package.archive_type, expected)
+
+    def test_unknown_archive(self):
+        self.package.upt_pkg.archives = [upt.Archive("url.co/dir/file.rar")]
+        expected = 'unknown'
+        self.assertEqual(self.package.archive_type, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
