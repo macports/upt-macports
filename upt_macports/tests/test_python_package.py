2c1d74ebd94ca41aea112381055d5057739111e1
Korusuke <karan.sheth@somaiya.edu>
2019-06-03 18:31:20 +0530

    Update python naming convention
    Naming convention followed is "py-<upstream_name>"
    
    Fixes #23

---
+++ upt_macports/tests/test_python_package.py
@@ -0,0 +1,22 @@
+import unittest
+
+import upt
+
+from upt_macports.upt_macports import MacPortsPythonPackage
+
+
+class TestMacPortsPythonPackage(unittest.TestCase):
+    def setUp(self):
+        self.package = MacPortsPythonPackage()
+        self.package.upt_pkg = upt.Package('test-pkg', '13.37')
+
+    def test_pkgname(self):
+        expected = ['py-foo', 'py-py-foo', 'py-pyfoo', 'py-pyFoo']
+        names = ['foo', 'py-foo', 'pyfoo', 'pyFoo']
+        for (name, expected_name) in zip(names, expected):
+            self.package.upt_pkg = upt.Package(name, '13.37')
+            self.assertEqual(self.package._pkgname(), expected_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
