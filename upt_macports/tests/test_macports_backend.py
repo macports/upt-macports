3815ccfbe3d399e497742312b529ef754484ea43
Korusuke <karan.sheth@somaiya.edu>
2019-08-08 23:29:55 +0530

    Standardize Perl versioning scheme for MacPorts
    MacPorts follows the "normalized, dotted-decimal form" versioning
    scheme, similar to what is obtained from:
    
      perl -Mversion -e 'print version->parse("<VERSION>")->normal'
    
    So we need do the same translation before checking if a package needs to
    be packaged.

--- upt_macports/tests/test_macports_backend.py
+++ upt_macports/tests/test_macports_backend.py
@@ -49,5 +49,41 @@ class TestMacPortsPackageExist(unittest.TestCase):
             self.macports_backend.package_versions('foo')
 
 
+class TestMacPortsCpanVersion(unittest.TestCase):
+    def setUp(self):
+        self.macports_backend = MacPortsBackend()
+        self.macports_backend.upt_pkg = upt.Package('foo', '42')
+        self.macports_backend.frontend = 'cpan'
+
+    def test_version_conversion(self):
+        converted = ['1', '1.2.3', '1.200.0', '1.200.0', '1.20.0',
+                     '1', '1.2.3', '1.200.0', '1.200.0', '1.20.0']
+        upstream = ['1', '1.2.3', '1.2', '1.20', '1.02',
+                    'v1', 'v1.2.3', 'v1.2', 'v1.20', 'v1.02']
+        for mp_ver, cpan_ver in zip(upstream, converted):
+            self.assertEqual(
+                    self.macports_backend.standardize_CPAN_version(mp_ver),
+                    cpan_ver)
+
+    @mock.patch('upt.Backend.needs_requirement')
+    def test_needs_requirement(self, mock_need_req):
+        specifiers = {
+            '>=42': '>=42',
+            '<=42': '<=42',
+            '!=42': '!=42',
+            '==42': '==42',
+            '>= 1.2, != 1.5, < 2.0': '>=1.200.0, !=1.500.0, <2.0.0',
+            '>= 1.2, != 2, < 3.0': '>=1.200.0, !=2, <3.0.0'
+        }
+
+        for key, value in specifiers.items():
+            req = upt.PackageRequirement('bar', key)
+            self.macports_backend.needs_requirement(req, 'fake-phase')
+            self.assertCountEqual(
+                mock_need_req.call_args[0][0].specifier.split(', '),
+                value.split(', ')
+                )
+
+
 if __name__ == '__main__':
     unittest.main()
